1270143a18aeb9625d882b19b1fee1ac4e318e56
Sean Tilson <seantilson@gmail.com>
2024-05-22 17:31:22 +0000

    change db, see if it breaks heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -79,15 +79,16 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''DATABASES = {
+'''
+DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}'''
+}
 
 
 # Password validation
