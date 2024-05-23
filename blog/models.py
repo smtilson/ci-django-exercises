8285bdd83cf02cc289d4d29e4bfe1cdc7200108e
Sean Tilson <seantilson@gmail.com>
2024-05-23 13:24:02 +0000

    issue with css on admin site

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 STATUS = ((0, "Draft"),(1,"Published"))
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -10,5 +11,6 @@ class Post(models.Model):
     content = models.TextField()
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
