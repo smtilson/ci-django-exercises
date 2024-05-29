3bc2a9375021649024fe359e97e3da58efe35029
Sean Tilson <seantilson@gmail.com>
2024-05-29 07:12:55 +0000

    env file not migrated to gitpod enterprise workspace. so all of those are broken and may lead to errors until that is fixed.
    add summernote to admin

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = '# os.environ.get("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django_summernote",
     'blog',
 ]
 
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
@@ -88,7 +89,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 '''
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+}'''
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
--- wookie_woo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WookieWooConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'wookie_woo'
